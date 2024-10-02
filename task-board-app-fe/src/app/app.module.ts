ca1d43b2d30e17e76acd957e6790b6e83d5e7049
mrBEASTBOY <hlinh0409@gmail.com>
2024-10-01 20:08:42 -0500

    submit update fe

--- task-board-app-fe/src/app/app.module.ts
+++ task-board-app-fe/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { TaskDetailsComponent } from './components/task-details/task-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    TaskDetailsComponent
   ],
   imports: [
     BrowserModule,
